c00e4a8b6da26517446e04b40f364d78cc8383fe
Hashblen <hashanulblennon@gmail.com>
2024-07-02 20:47:50 +0200

    Add Weapons, Items, Equipment (not fully deobf)

--- src/main.py
+++ src/main.py
@@ -3,3 +3,6 @@ from deobf import Deobf
 if __name__ == '__main__':
     Deobf("AchievementTemplateTb").save_deobfuscated(makeID="ID")
     Deobf("AvatarBaseTemplateTb").save_deobfuscated(makeID="ID")
+    Deobf("EquipmentSuitTemplateTb").save_deobfuscated(makeID="ID")
+    Deobf("ItemTemplateTb").save_deobfuscated(makeID="ID")
+    Deobf("WeaponTemplateTb").save_deobfuscated(makeID="ItemID")
